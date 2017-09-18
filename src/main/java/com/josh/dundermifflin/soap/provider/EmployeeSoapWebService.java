dcb215fe306d51e59094c480259e50622f70b7d2
Joshua Goldwasser <spearfish@berkeley.edu>
2017-09-18 09:43:41 -0700

    fixed soap web service issue, just forgot to import it into my applicationContext.xml file

--- src/main/java/com/josh/dundermifflin/soap/provider/EmployeeSoapWebService.java
+++ src/main/java/com/josh/dundermifflin/soap/provider/EmployeeSoapWebService.java
@@ -1,18 +1,32 @@
 package com.josh.dundermifflin.soap.provider;
 
+import java.util.List;
+
 import javax.jws.WebService;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
+
+import com.josh.dundermifflin.service.EmployeeService;
 import com.josh.dundermifflin.web.controller.model.EmployeeForm;
 
-@WebService
+@WebService // Makes the class behave as a SOAP provider
+@Service("EmployeeSoapWebService")
 public class EmployeeSoapWebService {
 	
-	public EmployeeForm findEmployeeByName(String name) {
-		EmployeeForm ef = new EmployeeForm();
-		ef.setName("Andy Bernard");
-		ef.setEmail("abernard@dm.com");
-		ef.setDepartment("Sales");
-		ef.setBranch("Scranton, PA");
-		return ef;
+	@Autowired
+	@Qualifier("EmployeeServiceImpl")
+	EmployeeService employeeService;
+	
+	public /*EmployeeForm*/ List<EmployeeForm> findEmployeeByName(String name) {
+//		EmployeeForm ef = new EmployeeForm();
+//		ef.setName("Andy Bernard");
+//		ef.setEmail("abernard@dm.com");
+//		ef.setDepartment("Sales");
+//		ef.setBranch("Scranton, PA");
+//		return ef;
+		List<EmployeeForm> efList = employeeService.findEmployeeByName(name);
+		return efList;
 	}
 }
